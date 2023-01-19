4e89848b57a7fc25ea6978c6d47f76791a557f52
Pratham Mishra <prathammishra178@gmail.com>
2023-01-19 08:57:39 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-    import "./App.css";
-    import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-    import React from "react";
-    import hero from "./components/hero/hero";
-    import Header from "./components/common/header/Header";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar } from "./components/NavBar";
+import { Banner } from "./components/Banner";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
-    function App() {
-    return (
-      <>
-        <Router>
-        <Header />
-          <Switch>
-            <Route exact path="/" component={hero} />
-          </Switch>
-        </Router>
-      </>
-    );
-    }
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
 
-    export default App;
+export default App;
