5eee726fbe8e5d2c4affef22425db6ba8b780217
Pratham Mishra <prathammishra178@gmail.com>
2023-01-19 22:22:46 +0530

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -74,13 +74,13 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
+                <a href="/#">
                   <img src={navIcon1} alt="" />
                 </a>
-                <a href="#">
+                <a href="/#">
                   <img src={navIcon2} alt="" />
                 </a>
-                <a href="#">
+                <a href="/#">
                   <img src={navIcon3} alt="" />
                 </a>
               </div>
