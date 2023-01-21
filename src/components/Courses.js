e2fc95c952207afce7ec337db8528439066878d4
Pratham Mishra <prathammishra178@gmail.com>
2023-01-22 02:23:20 +0530

    Update Courses.js

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -204,21 +204,73 @@
                     infinite={true}
                     className="owl-carousel owl-theme skill-slider"
                     >
-                    <div className="item">
-                        <img src={etc} alt="" />
-                        <h5>HOD</h5>
+                    <div className="item" style={{ paddingBottom: 60, paddingTop: 60, }}>
+                        <section className="parent">
+                        <div class="background">
+                            <div class="shape"></div>
+                            <div class="shape"></div>
+                        </div>
+
+                        <div class="wrapper">
+                            <div class="img-area">
+                            <div class="inner-area">
+                                <img
+                                style={{width: '100%'}}
+                                src="https://1.bp.blogspot.com/-NYPWByswrmg/YSKXi-CZiGI/AAAAAAAACZY/LtLeocLNtrkdb31nrCn6V0mkDRW-jhOUwCNcBGAsYHQ/s16000/img4.jpg"
+                                alt=""
+                                />
+                            </div>
+                            </div>
+
+                            <div class="name">First Year</div>
+
+                            {/* <div class="social-icons">
+            <a href="#" class="gl"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="facebook"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="insta"><i class="fab fa-google"></i></a> 
+            <a href="#" class="yt"><i class="fab fa fa-whatsapp"></i></a>
+        </div> */}
+
+                            <div class="buttons" style={{ width: '100%' }}>
+                            <button>View</button>
+                            {/* <button>Subscribe</button> */}
+                            </div>
+                        </div>
+                        </section>
                     </div>
-                    <div className="item">
-                        <img src={etc} alt="" />
-                        <h5>HOD</h5>
-                    </div>
-                    <div className="item">
-                        <img src={etc} alt="" />
-                        <h5>HOD</h5>
-                    </div>
-                    <div className="item">
-                        <img src={etc} alt="" />
-                        <h5>HOD</h5>
+                    <div className="item" style={{ paddingBottom: 60, paddingTop: 60 }}>
+                        <section className="parent">
+                        <div class="background">
+                            <div class="shape"></div>
+                            <div class="shape"></div>
+                        </div>
+
+                        <div class="wrapper">
+                            <div class="img-area">
+                            <div class="inner-area">
+                                <img
+                                style={{width: '100%'}}
+                                src="https://1.bp.blogspot.com/-NYPWByswrmg/YSKXi-CZiGI/AAAAAAAACZY/LtLeocLNtrkdb31nrCn6V0mkDRW-jhOUwCNcBGAsYHQ/s16000/img4.jpg"
+                                alt=""
+                                />
+                            </div>
+                            </div>
+
+                            <div class="name">Second Year</div>
+
+                            {/* <div class="social-icons">
+            <a href="#" class="gl"><i class="fab fa-youtube"></i></a>
+            <a href="#" class="facebook"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="insta"><i class="fab fa-google"></i></a> 
+            <a href="#" class="yt"><i class="fab fa fa-whatsapp"></i></a>
+        </div> */}
+
+                            <div class="buttons" style={{ width: '100%' }}>
+                            <button>View</button>
+                            {/* <button>Subscribe</button> */}
+                            </div>
+                        </div>
+                        </section>
                     </div>
                     </Carousel>
                 </div>
